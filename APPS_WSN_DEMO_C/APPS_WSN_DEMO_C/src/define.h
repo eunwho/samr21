560d2e772dd05153d6703aeb809b9303ee095b59
eunwho <eunwho@gmail.com>
2019-04-22 18:19:34 +0900

    add all

---
+++ APPS_WSN_DEMO_C/APPS_WSN_DEMO_C/src/define.h
@@ -0,0 +1,19 @@
+﻿/*
+ * define.h
+ *
+ * Created: 2019-04-22 오후 5:56:47
+ *  Author: ew2
+ */ 
+
+
+#ifndef DEFINE_H_
+#define DEFINE_H_
+
+
+
+#define MAX_RX_BUFFER_LENGTH   14
+#define  txd_en PORT->Group[0].OUTSET.reg=PORT_PA27
+#define  rxd_en PORT->Group[0].OUTCLR.reg=PORT_PA27
+
+
+#endif /* DEFINE_H_ */
---
+++ APPS_WSN_DEMO_C/APPS_WSN_DEMO_C/src/extern.h
@@ -0,0 +1,15 @@
+﻿/*
+ * extern.h
+ *
+ * Created: 2019-04-22 오후 5:58:46
+ *  Author: ew2
+ */ 
+
+
+#ifndef __EXTERN_H_
+#define __EXTERN_H_
+
+extern uint8_t zbeeSensState[10];
+extern uint8_t write_plc[17];
+
+#endif /* EXTERN_H_ */
